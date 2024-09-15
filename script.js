bc6e0a1f30a996bc51e0520184e583e2019e99e8
Jaylan Wong <jwong358cp@gmail.com>
2024-09-15 12:05:02 -0700

    SlideNoWork

---
+++ script.js
@@ -0,0 +1,5 @@
+// Toggle light/dark mode
+document.getElementById('theme-toggle').addEventListener('click', function() {
+    document.body.classList.toggle('light-mode');
+    document.body.classList.toggle('dark-mode');
+});
